3b699e44fcf033043534ca33dedb406772112ebf
LilNhan <doanngocnhanc3@gmail.com>
2024-08-07 13:41:51 +0700

    Move by WASD

---
+++ Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        // Lấy input từ bàn phím
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Tính toán vector di chuyển
+        Vector3 movement = new Vector3(horizontal, vertical, 0f);
+
+        // Di chuyển player
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+}
