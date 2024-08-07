51edbeefb10f2abe8b75449aa0b8c8206bbec985
LilNhan <doanngocnhanc3@gmail.com>
2024-08-07 21:09:36 +0700

    Shoot Systems

---
+++ Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform weaponTransform; // Tham chiếu đến GameObject vũ khí
+    public GameObject bulletPrefab; // Prefab của viên đạn
+    public float bulletSpeed = 10f; // Tốc độ di chuyển của viên đạn
+    public float cooldownTime = 0.5f; // Thời gian giữa các lần bắn
+
+    private float lastShootTime = 0f; // Thời gian bắn lần cuối
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time - lastShootTime >= cooldownTime)
+        {
+            ShootBullet();
+            lastShootTime = Time.time;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        // Tạo một instance mới của viên đạn
+        GameObject bullet = Instantiate(bulletPrefab, weaponTransform.position, Quaternion.Euler(0f, 0f, 90f));
+
+        // Đẩy viên đạn về phía trước
+        bullet.GetComponent<Rigidbody2D>().velocity = weaponTransform.right * bulletSpeed;
+    }
+
+    
+}
---
+++ Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WeaponControllerWithDrag : MonoBehaviour
+{
+    public Transform mainCharacter; // Tham chiếu đến Main Character
+    public float rotationSpeed = 5f; // Tốc độ xoay của vũ khí
+
+    private void Update()
+    {
+        RotateWeaponToMousePosition();
+        MoveWeaponWithCharacter();
+    }
+
+     void RotateWeaponToMousePosition()
+    {
+        // Lấy vị trí chuột trên màn hình
+        Vector3 mousePos = Input.mousePosition;
+
+        // Chuyển đổi vị trí chuột từ màn hình sang thế giới 2D
+        Vector3 worldPos = Camera.main.ScreenPointToRay(mousePos).origin;
+        worldPos.z = 0f; // Đảm bảo vũ khí chỉ xoay trên mặt phẳng 2D
+
+        // Tính toán góc xoay cần thiết để hướng vũ khí về phía chuột
+        Vector3 direction = (worldPos - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+     void MoveWeaponWithCharacter()
+    {
+       
+        transform.position = new Vector3(mainCharacter.position.x , mainCharacter.position.y +1f , transform.position.z);
+    }
+
+    
+}
